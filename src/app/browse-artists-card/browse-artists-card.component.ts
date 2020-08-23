37c8fcb99b47840a1330c61a4cad44ce6f82af0b
Duncan Proctor <duncanproctor@dmb.local>
2020-08-23 06:18:33 -0400

    scroll to top on navigation

--- src/app/browse-artists-card/browse-artists-card.component.ts
+++ src/app/browse-artists-card/browse-artists-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-browse-artists-card',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowseArtistsCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  async navigateToArtists(): Promise<void> {
+    await this.router.navigateByUrl('/top-artists');
+    await window.scrollTo(0, 0);
+  }
 }
